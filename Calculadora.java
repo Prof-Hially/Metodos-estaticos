cff85a4916ac9a5a8579f101d4d3d1e61c70180a
Prof-Hially <127833053+Prof-Hially@users.noreply.github.com>
2023-04-10 11:14:48 -0300

    Add files via upload

---
+++ Calculadora.java
@@ -0,0 +1,17 @@
+
+public class Calculadora {
+    
+    private double numero1;
+    private double numero2;
+    
+    //metodos estaticos
+
+    public static double soma(double n1, double n2){
+        double soma = n1 + n2;
+        return soma;
+    } 
+    public static double multi(double n1, double n2){
+        double multi = n1 * n2;
+        return multi;
+    } 
+}
---
+++ Main.java
@@ -0,0 +1,17 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+        double numero1 = 10;
+        double numero2 = 10;
+        
+       double soma = Calculadora.soma(numero1, numero2);
+       
+       System.out.println(soma);
+       
+       double mult = Calculadora.multi(numero1, numero2);
+       
+        System.out.println(mult);
+    }
+    
+}
